f11d48cbbe82553ce1f9982366bac0a41fabedc0
ShankarRaknahs <shankarsachin2u@gmail.com>
2021-02-25 14:15:43 +0530

    Review comment - Add config to centralize the color of the application https://github.com/pesto-students/little-tags-ShankarRaknahs-frontend/pull/6#discussion_r582550446

--- futurebasket/src/components/ImageCategorizer/index.js
+++ futurebasket/src/components/ImageCategorizer/index.js
@@ -3,30 +3,28 @@ import { makeStyles } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
 import Image from 'material-ui-image';
 import Button from '@material-ui/core/Button';
+import COLORS from '../../config/colors.config';
 
 const useStyles = makeStyles((theme) => ({
   button: {
     margin: theme.spacing(1),
     cursor: 'pointer',
-    background: 'linear-gradient(45deg, #00beff 30%, #00BEFF 90%)',
-    textAlign: 'right',
     fontWeight: '700',
     borderRadius: 5,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     fontSize: 'h6.fontSize',
     fontFamily: 'Monospace',
     visibility: 'hidden',
     '&:hover': {
       visibility: 'visible',
+      backgroundColor: COLORS.SECONDARY,
       '& $icon': {
-        color: '#000000',
+        color: COLORS.DARK,
       },
     },
   },
   icon: {
     visibility: 'visible',
-    color: '#00beff',
-    // color: 'secondary',
+    color: COLORS.SECONDARY,
   },
 }));
 
