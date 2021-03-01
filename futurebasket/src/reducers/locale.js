ed70d1e617431ad082a6d645feee43f0ba3d605d
Yogi Gachinmath <gachinmathyogi3@gmail.com>
2021-03-01 17:13:51 -0600

    Made the categeory component compatible with mobile

--- futurebasket/src/reducers/locale.js
+++ futurebasket/src/reducers/locale.js
@@ -1,5 +1,6 @@
 let locale = localStorage.getItem("language");
 if(!locale || !locale.includes["es", "en"]) locale = 'en';
+
 const localeReducer = (state = locale, action) => {
     console.log({state});
     switch (action.type) {
