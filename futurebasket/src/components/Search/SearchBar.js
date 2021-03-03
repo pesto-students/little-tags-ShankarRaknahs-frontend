22e5d92c9014d3dfdec735d3fcb9565a3daee39c
ShankarRaknahs <shankarsachin2u@gmail.com>
2021-03-03 23:15:57 +0530

    Fix Search bar padding issue - Issue#14

--- futurebasket/src/components/Search/SearchBar.js
+++ futurebasket/src/components/Search/SearchBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useStyles } from './styles';
-import { InputBase } from '@material-ui/core';
+import { Paper, IconButton, InputBase } from '@material-ui/core';
 
 import { Search } from '@material-ui/icons/';
 
@@ -8,18 +8,20 @@ const SearchBar = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.search}>
-      <div className={classes.searchIcon}>
-        <Search />
-      </div>
+    <Paper component='form' className={classes.root}>
       <InputBase
-        placeholder='Search…'
-        classes={{
-          input: classes.inputInput,
-        }}
-        inputProps={{ 'aria-label': 'search' }}
+        className={classes.input}
+        placeholder='Search...'
+        inputProps={{ 'aria-label': 'Search for products' }}
       />
-    </div>
+      <IconButton
+        type='submit'
+        className={classes.iconButton}
+        aria-label='search'
+      >
+        <Search />
+      </IconButton>
+    </Paper>
   );
 };
 
