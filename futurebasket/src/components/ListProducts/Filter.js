f8f52a6b0d3bd2b22407adf51e854b825ee8bb42
Yogi Gachinmath <gachinmathyogi3@gmail.com>
2021-03-13 18:32:16 -0600

    Updated Filter Functionality and Internalization

--- futurebasket/src/components/ListProducts/Filter.js
+++ futurebasket/src/components/ListProducts/Filter.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { Button, Divider, Slider, Typography } from "@material-ui/core";
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage } from "react-intl";
 import { useFilterStyles } from "./styles";
 import SearchFilters from "./SearchFilters";
 import { ItemsContext } from "../../pages/Search/Search";
@@ -8,7 +8,7 @@ import { ItemsContext } from "../../pages/Search/Search";
 function Filter({ productsData, setProductsData }) {
   const items = useContext(ItemsContext);
   const initialPrices = getInitialPriceRange(items);
-  
+
   const filterStyles = useFilterStyles();
 
   const [minMax, setminMax] = useState(initialPrices);
@@ -54,13 +54,19 @@ function Filter({ productsData, setProductsData }) {
     <div className={filterStyles.container}>
       <div className={filterStyles.subContainer}>
         <Typography variant="h4" className={filterStyles.filter}>
-          <FormattedMessage id = "items.filter" defaultMessage = "Filters"></FormattedMessage>
+          <FormattedMessage
+            id="items.filter"
+            defaultMessage="Filters"
+          ></FormattedMessage>
         </Typography>
         <Divider></Divider>
         <form onSubmit={handleSubmit}>
           <div className={filterStyles.slider}>
             <Typography id="range-slider" gutterBottom>
-              <FormattedMessage id = "items.priceRange" defaultMessage = "Price Range"></FormattedMessage>
+              <FormattedMessage
+                id="items.priceRange"
+                defaultMessage="Price Range"
+              ></FormattedMessage>
             </Typography>
             <Slider
               id="minMax"
@@ -94,8 +100,13 @@ function Filter({ productsData, setProductsData }) {
               setSelectedProductProperty: setSelectedSizes,
             }}
           />
-          <Button type="submit" variant="contained" color="primary" style={{ "margin-top": "1rem" }}>
-            Submit
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            style={{ "margin-top": "1rem" }}
+          >
+           <FormattedMessage id = "items.apply" defaultMessage = "Apply"></FormattedMessage> 
           </Button>
         </form>
       </div>
@@ -119,28 +130,37 @@ function applyFilters(
     }
     return false;
   };
+  const matchPrices = (product, minMax) => {
+    return product.price <= minMax[1] && product.price >= minMax[0];
+  };
+
+  const matchCompany = (product, selectedCompanies) => {
+    return (
+      selectedCompanies.length === 0 ||
+      selectedCompanies.includes(product.company)
+    );
+  };
+
+  const matchColorsSizes = (product, selected) => {
+    return selected.length === 0 || isPresent(selected, product.colors);
+  };
 
   let resIds = new Set();
+
   productsData.forEach((product) => {
-    if (product.price <= minMax[1] && product.price >= minMax[0])
+    if (
+      matchPrices(product, minMax) &&
+      matchCompany(product, selectedCompanies) &&
+      matchColorsSizes(product, selectedColors) &&
+      matchColorsSizes(product, selectedSizes)
+    )
       resIds.add(product.id);
-    else if (selectedCompanies.includes(product.company))
-      resIds.add(product.id);
-    else if (isPresent(selectedColors, product.colors)) resIds.add(product.id);
-    else if (isPresent(selectedSizes, product.sizes)) resIds.add(product.id);
   });
 
-  if (
-    resIds.size === 0 &&
-    !selectedColors.length &&
-    !selectedCompanies.length &&
-    !selectedSizes.length
-  )
-    return productsData;
   return productsData.filter((product) => resIds.has(product.id));
 }
 
 function getInitialPriceRange(products) {
-  const prices = products.map(product => product.price);
-  return [Math.min(...prices), Math.max(...prices)]; 
+  const prices = products.map((product) => product.price);
+  return [Math.min(...prices), Math.max(...prices)];
 }
